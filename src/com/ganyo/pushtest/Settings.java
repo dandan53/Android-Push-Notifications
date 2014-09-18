d62c3f1a24ef54c9b78decd53fbcfaaac5e4e62b
dandan53 <dandan53@gmail.com>
2014-09-18 15:32:23 +0300

    Android Push Notifications
    App which displays push notifications

---
+++ src/com/ganyo/pushtest/Settings.java
@@ -0,0 +1,22 @@
+package com.ganyo.pushtest;
+
+/** Change these values to match your setup! */
+public class Settings {
+
+	static final String UNASSIGNED_ORG_VALUE = "";
+	
+  // Google Client Id from Google API Console
+  static final String GCM_SENDER_ID = "317756875503";
+
+  // Notifier Name in App Services
+  static final String NOTIFIER = "google";
+
+  static final String API_URL = "https://api.usergrid.com";
+
+  static final String ORG = "eAngel";
+  static final String APP = "eAngelAndroid";
+
+  // set these if you want to use a user login
+  static final String USER = null;
+  static final String PASSWORD = null;
+}
